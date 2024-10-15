e36d95beb1522c9d022c176bacad552b7ff9d41a
이민형 <peterlee11@naver.com>
2024-10-15 17:30:37 +0900

    Prettier 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { useTodoList } from './queries'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const { data } = useTodoList()
+
+  console.log(data)
 
   return (
     <>
--- src/queries.ts
+++ src/queries.ts
@@ -1,24 +1,38 @@
-import { createQueryKeys } from "@lukemorales/query-key-factory";
-import axios from "axios";
+import { createQueryKeys } from '@lukemorales/query-key-factory'
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
 
-const BASE_URL = "https://my-json-server.typicode.com/1eeminhyeong/demo/";
+const BASE_URL = 'https://my-json-server.typicode.com/1eeminhyeong/demo/'
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   timeout: 30000,
-});
+})
 
 type TodoFilter = {
-  offset: number;
-  limit: number;
-};
+  offset?: number
+  limit?: number
+  done?: number
+}
+type Todo = {
+  id: number
+  title: string
+}
 
-const todoKeys = createQueryKeys("todo", {
+const todoKeys = createQueryKeys('todo', {
   all: null,
   list: (filter: TodoFilter) => ({
     queryKey: [{ filter }],
     queryFn: async () => {
-      const [] = await Promise.all([axiosInstance.get("list")]);
+      const [list, count] = await Promise.all([
+        axiosInstance.get<Todo[]>('list', { params: filter }),
+        axiosInstance.get('count'),
+      ])
+
+      console.log(list.data, count.data)
     },
   }),
-});
+})
+
+export const useTodoList = () =>
+  useQuery(todoKeys.list({ offset: 0, limit: 10 }))
